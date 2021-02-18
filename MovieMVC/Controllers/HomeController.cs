e30367435b867e7195707421d7dde2b9b5b53111
Ramon <rag4@pdx.edu>
2021-02-17 21:39:03 -0800

    initial commit

---
+++ MovieMVC/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieMVC.Models;
+
+namespace MovieMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Movies()
+        {
+            return View();
+        }
+
+        public IActionResult Result(Result r)
+        {
+            if(r.title.Length > 50)
+            {
+                TempData["msg"] = "<script>alert('Movie Title too long.');</script>";
+                return View("Movies");
+            }
+
+            if(r.year < 1880)
+            {
+                TempData["msg"] = "<script>alert('Movie year before 1880.');</script>";
+                return View("Movies");
+            }
+
+            int currentDate = int.Parse(DateTime.Now.Year.ToString());
+            if(r.year > currentDate)
+            {
+                TempData["msg"] = "<script>alert('Movie year after current year.');</script>";
+                return View("Movies");
+            }
+
+            return View(r);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MovieMVC/Models/Result.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieMVC.Models
+{
+    public class Result
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string genre { get; set; }
+        public int year { get; set; }
+        public string actors { get; set; }
+        public string directors { get; set; }
+    }
+}
